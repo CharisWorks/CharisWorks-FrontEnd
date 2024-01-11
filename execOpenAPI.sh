9c8220424476a1d3482b9a8b647a74fa6631cf1d
whatacotton <whatacotton@gmail.com>
2024-01-11 22:22:39 +0900

    fix: last empty row

--- execOpenAPI.sh
+++ execOpenAPI.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 docker run -d --init --rm -v $(pwd):/tmp -p 4010:4010 stoplight/prism:4 mock -h 0.0.0.0 "/tmp/openapi.yml"
+
 ID=(`docker container ps -f ancestor=stoplight/prism:4 --format "table {{.ID}}" `)
 echo "Container ID:"${ID[2]}
 echo "モックサーバーが起動しました。"
+
 Query=(`docker inspect ${ID[2]} | grep \"IPAddress\"\:`)
 IP=(`echo ${Query[1]}| sed 's/^.*"\(.*\)".*$/\1/'`)
+
 echo  "http://"$IP"でアクセスできます。"
 read -p "Enterでサーバーを終了します"
 docker stop ${ID[2]}
