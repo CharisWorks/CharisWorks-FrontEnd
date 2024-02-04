9f9bc4fdcd08c6d0962ae4357a85e317d5dcafb1
WhatACotton <whatacotton@gmail.com>
2024-02-04 18:16:57 +0900

    add: transaction

--- src/app/api/transaction.ts
+++ src/app/api/transaction.ts
@@ -2,19 +2,38 @@ import axios, { AxiosResponse } from 'axios'
 
 const ADDRESS: string | undefined = process.env.NEXT_PUBLIC_SERVER_ADDRESS
 
-type Transaction = {}
-type TransactionList = {
-    Transaction: Transaction[]
+type Transaction = {
+    transaction_id: string
+    tracking_id: string
+    items: Items[]
 }
-type TransactionDetail = {}
+type TransactionDetail = {
+    transaction_id: string
+    tracking_id: string
+    address: {
+        real_name: string
+        zip_code: string
+        address: string
+        phone_number: string
+    }
+    items: Items[]
+}
 
-const GetTransaction = async (): Promise<TransactionList> => {
+type Items = {
+    item_id: string
+    quantity: number
+    properties: {
+        name: string
+        price: number
+    }
+}
+const GetTransaction = async (): Promise<Transaction[]> => {
     const URL = ADDRESS + '/user/transaction'
     const response = await axios.get(URL)
     if (response.status != 200) {
         throw new Error(response.data.json().message)
     }
-    const data: TransactionList = await response.data.json()
+    const data: Transaction[] = await response.data.json()
     return data
 }
 
