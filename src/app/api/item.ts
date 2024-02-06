75bbbe8b6ae4cd114f1c309f845c2efde71ef96c
WhatACotton <whatacotton@gmail.com>
2024-02-06 17:15:05 +0900

    fix: replace fetch request of item

--- src/app/api/item.ts
+++ src/app/api/item.ts
@@ -3,22 +3,42 @@ import { ItemPreview, ItemDetail, IItemRequests } from "./interfaces";
 const ADDRESS: string | undefined = process.env.NEXT_PUBLIC_SERVER_ADDRESS
 
 class ItemRequests implements IItemRequests {
+    private url = new URL(process.env.NEXT_PUBLIC_SERVER_ADDRESS ? process.env.NEXT_PUBLIC_SERVER_ADDRESS : "http://localhost")
     Get = async (): Promise<ItemPreview[] | null> => {
-        const url = ADDRESS + '/item'
-        const response = await axios.get(url)
-        const data: ItemPreview[] = await response.data.json()
+        this.url.pathname = '/api/cart'
+        const response = await fetch(this.url.toString(), {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data: ItemPreview[] = await response.json()
         return data
     }
     GetKeyword = async (keyword: string): Promise<ItemPreview[] | null> => {
-        const url = ADDRESS + '/item/search?keyword=' + keyword
-        const response = await axios.get(url)
-        const data: ItemPreview[] = await response.data.json()
+        this.url.pathname = '/item/search'
+        this.url.searchParams.set("itemId", keyword)
+        const response = await fetch(this.url.toString(), {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data: ItemPreview[] = await response.json()
         return data
     }
     GetDetail = async (item_id: string): Promise<ItemDetail> => {
-        const url = ADDRESS + '/item/' + item_id
-        const response = await axios.get(url)
-        const data: ItemDetail = await response.data.json()
+        this.url.pathname = '/item/' + item_id
+        const response = await fetch(this.url.toString(), {
+            method: "GET",
+            mode: "cors",
+            headers: {
+                "Content-Type": "application/json",
+            }
+        });
+        const data: ItemDetail = await response.json()
         return data
     }
 }
