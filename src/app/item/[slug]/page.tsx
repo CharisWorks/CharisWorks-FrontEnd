201bac2579d337f99da36201572eef5690fec0e0
WhatACotton <whatacotton@gmail.com>
2024-02-14 17:14:58 +0900

    add: impl of item

--- src/app/item/[slug]/page.tsx
+++ src/app/item/[slug]/page.tsx
@@ -1,9 +1,21 @@
 'use client'
 
+import { FetchRequests } from '@/app/api/fetch'
+import { ItemRequests } from '@/app/api/item'
+import { ItemDetail } from '@/app/api/models/item'
 import { useParams } from 'next/navigation'
+import { useEffect, useState } from 'react'
 
 const ItemPage = () => {
   const params = useParams<{ slug: string }>()
+  const ItemRequest = new ItemRequests(new FetchRequests())
+  const [Item, setItem] = useState<ItemDetail | undefined>(undefined)
+  useEffect(() => {
+    ;(async () => {
+      const item = await ItemRequest.GetDetail(params.slug)
+      setItem(item)
+    })()
+  }, [])
 
   // Route -> /shop/[tag]/[item]
   // URL -> /shop/shoes/nike-air-max-97
