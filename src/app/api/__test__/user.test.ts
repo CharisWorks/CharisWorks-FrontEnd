a0cd68143c5a44d250e274378a45c1cea3c8f5b5
WhatACotton <whatacotton@gmail.com>
2024-02-25 22:00:47 +0900

    add: e2e test for stripe error in get accout without address register

--- src/app/api/__test__/user.test.ts
+++ src/app/api/__test__/user.test.ts
@@ -1,18 +1,11 @@
 //@ts-ignore
 import { expect, test } from "bun:test";
-
-import { FirebaseApp, initializeApp } from "firebase/app";
-import { connectAuthEmulator, createUserWithEmailAndPassword, getAuth, signInWithEmailAndPassword } from "firebase/auth";
-import { FirebaseRequestImpl, ItemRequestImpl, UserRequestImpl } from "../lib/firebase";
-import { UserRequests } from "../user";
-import { AuthFetchRequests, FetchRequests } from "../fetch";
-import { ItemRequests } from "../item";
+import { FirebaseRequestImpl, ManufacturerRequestImpl, UserRequestImpl } from "../lib/firebase";
 import { auth } from "../firebase";
 
 test("email is not verified", async () => {
     await FirebaseRequestImpl.SignInWithEmail(auth, "hoge@example.com", "example")
     const idToken = await auth.currentUser?.getIdToken()
-    console.log("idToken: ", idToken)
     if (idToken) {
         const user = UserRequestImpl(idToken)
         await user.Get().catch((error) => {
@@ -29,3 +22,47 @@ test("SignIn fail", async () => {
         expect(error.code).toBe("auth/invalid-credential")
     })
 });
+test("create user", async () => {
+    await FirebaseRequestImpl.SignInWithEmail(auth, "cowatanabe26@gmail.com", "example")
+    const idToken = await auth.currentUser?.getIdToken()
+    if (idToken) {
+        const user = UserRequestImpl(idToken)
+        await user.Get().catch((e) => {
+            expect(e.message).toBe("creating user for DB")
+        })
+
+    } else {
+        throw new Error("idToken is null")
+    }
+})
+
+test("get user", async () => {
+    await FirebaseRequestImpl.SignInWithEmail(auth, "cowatanabe26@gmail.com", "example")
+    const idToken = await auth.currentUser?.getIdToken()
+    if (idToken) {
+        const user = UserRequestImpl(idToken)
+        const user_data = await user.Get().catch((e) => {
+            expect(e.message).toBe("creating user for DB")
+        })
+        if (user_data) {
+            expect(user_data.user_id).toBe("WQElviFCW3TEV77prNZB7Q2TwGt2")
+        } else {
+            throw new Error("user_data is null")
+        }
+    } else {
+        throw new Error("idToken is null")
+    }
+})
+//住所登録あたりの処理がまだなので先送り
+test("住所が登録されていないのに口座登録しようとしたらエラー", async () => {
+    await FirebaseRequestImpl.SignInWithEmail(auth, "cowatanabe26@gmail.com", "example")
+    const idToken = await auth.currentUser?.getIdToken()
+    if (idToken) {
+        const manufacturer = ManufacturerRequestImpl(idToken)
+        const res = await manufacturer.CreateAccountRequest().catch((e) => {
+            expect(e.message).toBe("住所が登録されていません。")
+        })
+    } else {
+        throw new Error("idToken is null")
+    }
+})
