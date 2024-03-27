8e847cc4c26cda5643e189ffd9afc16d43da389b
whatacotton <whatacotton@gmail.com>
2024-03-28 03:16:23 +0900

    add: example form

--- src/app/(mypage)/mypage/address/register/page.tsx
+++ src/app/(mypage)/mypage/address/register/page.tsx
@@ -1,4 +1,47 @@
-const Register = () => {
-  return <div>Register</div>
+'use client'
+import { useForm } from 'react-hook-form'
+import {
+  FormErrorMessage,
+  FormLabel,
+  FormControl,
+  Input,
+  Button,
+} from '@chakra-ui/react'
+import { useEffect, useState } from 'react'
+
+export default function Register() {
+  const {
+    handleSubmit,
+    register,
+    formState: { errors, isSubmitting },
+  } = useForm()
+  const [error, setError] = useState<string>('')
+  function onSubmit(values: any) {
+    console.log(values)
+  }
+  useEffect(() => {
+    if (errors.name?.message) {
+      setError(errors.name.message.toString())
+    }
+  }, [errors.name])
+
+  return (
+    <form onSubmit={handleSubmit(onSubmit)}>
+      <FormControl isInvalid={errors.name?.message ? true : false}>
+        <FormLabel htmlFor="name">First name</FormLabel>
+        <Input
+          id="name"
+          placeholder="name"
+          {...register('name', {
+            required: 'This is required',
+            minLength: { value: 4, message: 'Minimum length should be 4' },
+          })}
+        />
+        <FormErrorMessage>{errors.name?.message?.toString()}</FormErrorMessage>
+      </FormControl>
+      <Button mt={4} colorScheme="teal" isLoading={isSubmitting} type="submit">
+        Submit
+      </Button>
+    </form>
+  )
 }
-export default Register
